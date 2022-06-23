8f61fca3be9ec086e7708b6f0c4341b37852c86b
Elvis Haruto Sánchez Jiménez <XxHaRuToxX@localhost.localdomain>
2022-06-23 12:41:45 -0500

    updated

---
+++ src/Componentes/slider/Slider.jsx
@@ -0,0 +1,71 @@
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { NewsContext } from '../../NewContext';
+import './slider.css';
+
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FFBB28", "#FFBB28"];
+const delay = 2500;
+
+function Slideshow() {
+    const [index, setIndex] = useState(0);
+    const timeoutRef = useRef(null);
+    const { slideFiveNews } = useContext(NewsContext);
+
+    console.log(slideFiveNews[slideFiveNews.length])
+
+    function resetTimeout() {
+        if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+        }
+    }
+
+    useEffect(() => {
+        resetTimeout();
+        timeoutRef.current = setTimeout(
+        () =>
+            setIndex((prevIndex) =>
+            prevIndex === 5 - 1 ? 0 : prevIndex + 1
+            ),
+        delay
+        );
+
+        return () => {
+        resetTimeout();
+        };
+    }, [index]);
+
+    return (
+        <div className="div_slidershow">
+            <div className="slideshow">
+                <div
+                    className="slideshowSlider"
+                    style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+                >
+                    {slideFiveNews.articles && slideFiveNews.articles.map((news, index) => (
+                        <div className="slide" key={index}>
+                            <img
+                                src={news.urlToImage}
+                            />
+                            <div className="news_five_title">
+                                <h5>{news.title}</h5>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="slideshowDots">
+                    {colors.map((_, idx) => (
+                    <div
+                        key={idx}
+                        className={`slideshowDot${index === idx ? " active" : ""}`}
+                        onClick={() => {
+                        setIndex(idx);
+                        }}
+                    ></div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Slideshow;
--- src/NewContext.js
+++ src/NewContext.js
@@ -4,18 +4,46 @@ import axios from "axios";
 export const NewsContext = createContext();
 
 export const NewsContextProvider = (props) => {
-     const [data, setData] = useState();
-     const apiKey = '3bec5be64c3849c68b276b605ccaefb4';
+     const [getData, setGetData] = useState([]);
+     const [slideFiveNews, setSlideFiveNews] = useState([]);
+     const apiKey = '5e092c2e0a6d494f8f633cd145b5b8b1';
+     const date = new Date().toLocaleDateString().split('/').reverse().join('-'); 
+
+     useEffect(()=>{
+          const getFiveNews = async()=>{
+               try {
+                    const news = await axios.get(`https://newsapi.org/v2/everything?q=peru&from=${date}&pageSize=5&sortBy=popularity&apiKey=${apiKey}`)
+                    const newsData = await news.data;
+                    setSlideFiveNews(newsData);
+                    console.log(newsData.articles)
+               } catch (error) {
+                    console.log(error)
+               }
+               
+               }
+          getFiveNews()
+     },[])
 
      useEffect(() => {
-         axios.get( `https://newsapi.org/v2/everything?q=tesla&from=2022-05-20&sortBy=publishedAt&apiKey=${apiKey}` 
-         )
-          .then(response => setData(response.data))
-          .catch( error => console.log(error));  
+          const getNews = async()=>{
+               try {
+                    const news = await axios.get(`https://newsapi.org/v2/everything?q=us&apiKey=${apiKey}`)
+                    const newsData = await news.data;
+                    setGetData(newsData)
+                    console.log(newsData.articles);
+               } catch (error) {
+                    console.log(error)
+               }
+               
+          }
+         getNews();
      },[]) 
 
      return(
-        <NewsContext.Provider value = {{data}}>
+        <NewsContext.Provider value={{
+          getData,
+          slideFiveNews
+        }}>
              {props.children} 
         </NewsContext.Provider>
      )
