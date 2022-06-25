57093560b47f27a0bb9257c77a3c0538c45a2133
Fercapullin <belizariof22@gmail.com>
2022-06-25 00:07:38 -0500

    Mejoras de estilos

--- src/Componentes/navbar/Navbar.jsx
+++ src/Componentes/navbar/Navbar.jsx
@@ -7,23 +7,20 @@ import '../../Css/cabecera.css'
 export const Navbar = () => {
     return (
         <nav className="header">
+            
             <NavLink className="logo" to={"/"}><div className="div_news">News <span className="span_news">App</span></div></NavLink>
-            <div>
-                <Link className="category_button" to={"business"}>Negocios</Link>
-                <Link className="category_button" to={"entertainment"}>Entretenimiento</Link>
-                <Link className="category_button" to={"general"}>General</Link>
-                <Link className="category_button" to={"health"}>Salud</Link>
-                <Link className="category_button" to={"science"}>Ciencia</Link>
-                <Link className="category_button" to={"sports"}>Deportes</Link>
-                <Link className="category_button" to={"technology"}>Tecnologías</Link>
-            </div>
-            <ul className="main-menu">
-            
-                <Link to="login" className="button"> Iniciar Sesion </Link>
-                <Link to="registro" className="button"> Registrate </Link>
-            
-            <i className="menu-icono fa-solid fa-bars"></i>
+            <i className="search fa-solid fa-magnifying-glass"></i>
+            <ul>
+                 <li>  <Link className="category_button" to={"business"}>Negocios</Link> </li>
+                 <li>  <Link className="category_button" to={"entertainment"}>Entretenimiento</Link> </li>
+                 <li>  <Link className="category_button" to={"general"}>General</Link> </li>
+                 <li>  <Link className="category_button" to={"health"}>Salud</Link> </li>
+                 <li>  <Link className="category_button" to={"science"}>Ciencia</Link> </li>
+                 <li>  <Link className="category_button" to={"sports"}>Deportes</Link></li>
+                 <li>  <Link className="category_button" to={"technology"}>Tecnologías</Link></li>
+                 <li>  <Link to="login" className="category_button"> Iniciar Sesion </Link>    </li>       
             </ul>
+            <i className=" fa-solid fa-bars"></i>
         </nav>
     )
 }
--- src/Login/AppLogin.jsx
+++ src/Login/AppLogin.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+
+/*import React, { useState, useEffect } from "react";
 import Identity from "@arc-publishing/sdk-identity";
 import { Link } from "react-router-dom";
 import '../Css/Login.css'
@@ -87,3 +88,133 @@ export const AppLogin = () => {
     </>
   );
 };
+*/
+import React, { useState, useEffect } from "react";
+import Identity from "@arc-publishing/sdk-identity";
+import { Link } from "react-router-dom";
+import '../Css/Login.css';
+
+export const AppLogin = () => {
+    
+  const urlBase = "https://api-sandbox.elcomercio.pe";
+  const [user, setUser] = useState(false);
+  const [error, setError] = useState(false);
+  const [dataLogin, setdataLogin] = useState({
+    emailLogin: "",
+    passwordLogin: "",
+  });
+  //cuando carge la apk cargara la libreria de Identity
+  useEffect(() => {
+    Identity.apiOrigin = urlBase;
+  });
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setdataLogin({
+      // ... hace una copia
+      ...dataLogin,
+      [name]: value,
+    });
+  };
+
+  const handleClick = () => {
+    const { emailLogin, passwordLogin } = dataLogin;
+    //metodo dentro de la libreria
+    Identity.login(emailLogin, passwordLogin)
+      .then((res) => {
+        setUser(true);
+      })
+      .catch((error) => {
+        setError(
+          "El Correo o contraseña que ha proporcionado no son correctos. Inténtelo de nuevo."
+        );
+      });
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+  };
+
+
+    return (
+        
+       
+        <div className="general_div_login">
+            <div className="container_login">
+            <input type="checkbox" id="flip"/>
+            <div className="cover">
+            <div className="front">
+                <img className="img_login" src="https://images.pexels.com/photos/3585074/pexels-photo-3585074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                <div className="text">
+                <span className="text-1">Tus noticias a tiempo real  <br/>Siempre activo</span>
+                <span className="text-2">Conoce mas noticias</span>
+                </div>
+            </div>
+            <div className="back">
+                <img className="backImg img_login" src="https://images.pexels.com/photos/3584993/pexels-photo-3584993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                <div className="text">
+                <span className="text-1">Complete miles of journey <br/> with one step</span>
+                <span className="text-2">Let's get started</span>
+                </div>
+            </div>
+            </div>
+            <div className="forms">
+                <div className="form-content">
+                <div className="login-form">
+                
+                    {user ? (
+                        <h1 className="bienvenida">Bienvenido a tu cuenta</h1>
+                    ) : (
+                      <>
+                      <div className="title">Login</div>
+                      <form >
+                    <div className="input-boxes">
+                    <div className="input-box">
+                        <i className="fas fa-envelope"></i>
+                        <input onChange={handleChange} type="text" placeholder="Ingresa tu Correo" name="emailLogin" required/>
+                    </div>
+                    <div className="input-box">
+                        <i className="fa-solid fa-lock"/>
+                        <input onChange={handleChange}  type="password" placeholder="Ingresa tu Contraseña" name="passwordLogin" required/>
+                    </div>
+                    <div className="text"><a href="#">Olvidaste tu contraseña?</a></div>
+                    <div className="button_login input-box">
+                        <input type="submit" value="Ingresar" onClick={handleClick} name="btnLogin"/>
+                    </div>
+                    <div className="text sign-up-text">No tienes una cuenta? <label htmlFor="flip">Registrate!</label></div>
+                    {error && <p> {error} </p>}
+                    </div>
+                </form>
+                      </>
+                
+                    )}
+            </div>
+                <div className="signup-form">
+                <div className="title">Registrarse</div>
+                <form action="#">
+                    <div className="input-boxes">
+                    <div className="input-box">
+                        <i className="fas fa-user"></i>
+                        <input type="text" placeholder="Ingresa tu nombre" required/>
+                    </div>
+                    <div className="input-box">
+                        <i className="fas fa-envelope"></i>
+                        <input type="text" placeholder="Ingresa tu correo" required/>
+                    </div>
+                    <div className="input-box">
+                        <i className="fas fa-lock"></i>
+                        <input type="password" placeholder="Ingresa tu contraseña" required/>
+                    </div>
+                    <div className="button_login input-box">
+                        <input type="submit" value="Registrarse"/>
+                    </div>
+                    <div className="text sign-up-text">Ya tienes una cuenta? <label htmlFor="flip">Logeate</label></div>
+                    </div>
+            </form>
+            </div>
+            </div>
+            </div>
+        </div>
+      </div>
+    )
+}
